cd340538214b826c7f78fc353882c1dbe564bfb4
Bellaaspera <lanasenatorova@gmail.com>
2023-07-03 23:28:25 +0300

    Initial Commit

---
+++ ColorizedAppSwiftUITest/ColorizedAppSwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUITestApp.swift
+//  ColorizedAppSwiftUITest
+//
+//  Created by Светлана Сенаторова on 03.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
